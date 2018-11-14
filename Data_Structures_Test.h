492ae1f78e8e69a8fab0a43247a18d86dc5e2ef2
Mohammad Sedighi <44990933+msedighi@users.noreply.github.com>
2018-11-14 11:28:40 -0800

    Adding test files for Embedding Dimension function

---
+++ Data_Structures_Test.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef DATA_STRUCTURES_TEST_H
+#define DATA_STRUCTURES_TEST_H
+#endif // !1
+
+int Embedding_Dimension_Test1();
+int Embedding_Dimension_Test2();
+int Embedding_Dimension_Test3(int num_points);
+
